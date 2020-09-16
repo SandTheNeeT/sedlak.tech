d7baad512013de31ed2ecb830403420c5382a44f
SandTheNeeT <novakpetr2002@gmail.com>
2020-09-16 14:26:00 +0200

    Hello world!

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Landing Page">
+        <meta name="keywords" content="Gaming">
+        <meta name="author" content="SandTheNeeT_">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+
+        <title>sndcrw.cz</title>
+    </head>
+
+    <body>
+        <p>Hello World!</p>
+    </body>
+
+</html>
