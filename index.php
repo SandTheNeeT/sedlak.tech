f3f77b174ac2c1241e6eebcd811756b3aa3c95ce
SandTheNeeT <novakpetr2002@gmail.com>
2020-09-16 19:50:08 +0200

    Added FA icons and links

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
         <meta name="keywords" content="Gaming">
         <meta name="author" content="SandTheNeeT_">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link rel="stylesheet" href="style.css">
+        <link href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all" rel="stylesheet">
+        <link href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all" rel="stylesheet">
+        <link href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all" rel="stylesheet">
 
         <title>sndcrw.cz</title>
     </head>
@@ -22,12 +26,17 @@
             <div class="navr">
                 <table>
                     <tr>
-                        <th class="navr">Hello</th>
-                        <th class="navr">World</th>
-                        <th class="navr">!</th>
+                        <th class="navrActive"><a href="#"><i class="fas fa-home"></i></a></th>
+                        <th class="navr"><a href="https://discord.gg/QT3ugYH" target="_blank"><i class="fab fa-discord"></i></a></th>
+                        <th class="navr"><a href="ts3server://ts.sndcrw.cz"><i class="fab fa-teamspeak"></i></a></th>
+                        <th class="navr"><a href="https://amp.sndcrw.cz" target="_blank"><i class="fas fa-server"</i></a></th>
                     </tr>
                 </table>
             </div>
+        </div>
+
+        <div class="a">
+            <p>&nbsp;</p>
         </div>
     </body>
 
