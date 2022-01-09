e30f4b9861feaea0180b392569d9b570d4bbac2b
Petr Sedlák <sandtheneet@warfaremc.eu>
2022-01-09 01:06:10 +0100

    code cleanup

--- contact.php
+++ contact.php
@@ -10,7 +10,6 @@
 
         <link rel="stylesheet" href="lib/css/fonts.css">
         <link rel="stylesheet" href="lib/css/style.css">
-        <link rel="stylesheet" href="lib/css/style2.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="icon" type="image/png" href="lib/img/favicon.png">
 
